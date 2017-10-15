53f70588b624400aaf9d1e366dc74a1d1f04861b
Nicolas Carella <esep642@gmail.com>
2017-10-15 13:59:57 -0300

    El juego como quedo cuando me fui de clase.

---
+++ 5enRaya-master/5EnRaya/Assets/Scripts/Model/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game
+{
+    private List<Play> Plays;
+    private Board board;
+    private Player[] players;
+    private int turn = 0;
+    public Game(Board board, Player[] players)
+    {
+        this.board = board;
+        this.players = players;
+    }
+
+    public Board Board { get { return board; } }
+    public IEnumerable<Player> Players { get { return players; } }
+    public int Turn { get { return turn; } }
+    public Player CurrentPlayer { get { return players[turn]; } }
+    
+    public int IndexOfPlayer(Player player)
+    {
+        return Array.IndexOf(players, player);
+    }
+
+    public void Play(int column)
+    {
+        try
+        {
+            Plays.Add(new Play(players[turn],players[(players.Length)-turn],column));
+            CurrentPlayer.Play(column, board);
+            NextTurn();
+        }
+        catch (InvalidOperationException)
+        {
+            // Do nothing
+        }
+    }
+
+    private void NextTurn()
+    {
+        turn = (turn + 1) % players.Length;
+    }
+}
---
+++ 5enRaya-master/5EnRaya/Assets/Scripts/ParaCommand/Play.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play  {
+    public Play(Player doer, Player other, int col)
+    {
+        Doer = doer;
+        Other = other;
+        Col = col;
+    }
+    private Player Doer;
+    private Player Other;
+    private int Col;
+    public int col()
+    {
+        return Col;
+    }
+
+    public void Do ()
+    {
+        
+    }
+    public void UnDo()
+    {
+
+    }
+}
